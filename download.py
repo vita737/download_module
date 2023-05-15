575922b96d10febd29a3e53f363b51602ec5e6d7
vita737 <121865691+vita737@users.noreply.github.com>
2023-05-15 14:51:53 +0530

    ghjgj

--- download.py
+++ download.py
@@ -23,7 +23,7 @@ def start_download():
 
 def resume_download(file_url, file_name):
 	file_byte = os.path.getsize(file_name) # Get the size of file downloading to get continuation location
-	resume_download_header = {'Range':'bytes='+file_byte+'-'} # The Range HTTP request header indicates the part of a document that the server should return Range: <unit>=<range-start>-
+	resume_download_header = {'Range':'bytes='+str(file_byte)+'-'} # The Range HTTP request header indicates the part of a document that the server should return Range: <unit>=<range-start>-
 	open(file_name,'ab').write(requests.get(file_url,headers=resume_download_header, stream = True).content) # headers specify additional data for requests
 def start_resume():
 	file_url_input2 = input('Enter the file www url: ')
