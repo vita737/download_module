63e37bad4882c1065f139e1be00a8344a59f385e
vita737 <121865691+vita737@users.noreply.github.com>
2023-06-29 12:41:59 +0530

    A good version.

--- raw_download.py
+++ raw_download.py
@@ -1,10 +1,27 @@
-# Features : Faster than featured download, don't show file progress anyway, downloads actual content when failed
+import requests # HTTP library to send HTTP request
+import os # Module to use operating system dependent functionality
+import sys
 
-import requests, time
-s=time.time()
-r=requests.get('http://212.183.159.230/100MB.zip',stream=True)
-op=open('new.zip','wb')
-for i in r.iter_content(chunk_size=1024):
-    op.write(i)
-e=time.time()
-print(e-s)
+
+def download_new_file(file_url, file_name):
+	print('Downloading a new file')
+	try:
+		response = requests.get(file_url, stream=True)
+		for x in response.iter_content(chunk_size=1024): #  iterate over each bit in response.content, chunk_size tells how much to put in memory at once 
+			open(file_name,'ab').write(x) # create the file, write to it binary from the url, stream=true meanswrite directly	
+		if sys.getsizeof(open(file_name,'rb').read()) == response.headers['Content-Length']: # if full file is downloaded
+			print("\nsuccessfully downloaded !")
+		else:
+			print("Failed, Possibly because the network has gone.")			
+	except Exception as error:
+		if type(error).__name__ == 'SSLError': # check error name
+			print('SSLError: Possibly because network is not being distributed properly, try closing all your network consuming software.')
+
+
+def start_download():
+	global file_url_input, file_name_input
+	file_url_input = input('Enter the file www url: ')
+	file_name_input = input('Enter the file name in local storage: ')
+	download_new_file(file_url_input, file_name_input)
+
+start_download() # do it
--- resume_download.py
+++ resume_download.py
@@ -2,24 +2,23 @@ import requests # HTTP library to send HTTP request
 import os # Module to use operating system dependent functionality
 
 def resume_download(file_url, file_name):
-	file_byte = os.path.getsize(file_url) # Get the size of file downloading to get continuation location
-	resume_download_header = {'Range':'bytes='+file_byte+'-'} # The Range HTTP request header indicates the part of a document that the server should return Range: <unit>=<range-start>-
-	open(file_name,'ab').write(requests.get(file_url,headers=resume_download_header, stream= True).content) # create the file, append to it binary from the url, stream=true means write directly, headers specify additional data for requests
+	file_byte = os.path.getsize(file_name) # Get the size of file downloading to get continuation location
+	resume_download_header = {'Range':'bytes='+str(file_byte)+'-'} # The Range HTTP request header indicates the part of a document that the server should return Range: <unit>=<range-start>-
+	try:
+		response =requests.get(file_url, headers=resume_download_header, stream= True)
+		for x in response.iter_content(chunk_size=1024): #  iterate over each bit in response.content, chunk_size tells how much to put in memory at once 
+			open(file_name,'ab').write(x) # create the file, append to it binary from the url, stream=true means write directly, headers specify additional data for requests
+			if file_byte == response.headers['Content-Length']: # check lenght
+				print("Downloaded successfully")
+			else:
+				print("Failed for some reason")
+	except Exception as error:
+		if type(error).__name__ == 'SSLError': # check error name
+			print('SSLError: Possibly because network is not being distributed properly, try closing all your network consuming software.')
 
 def start_resume():
 	file_url_input2 = input('Enter the file www url: ')
 	file_name_input2 = input('Enter the file name in local storage: ')
-	download_new_file(file_url_input2, file_name_input2)
+	resume_download(file_url_input2, file_name_input2)
 
-i=1 # index for location tracking of condition
-b=0 #index 0,1 which tells if want to run loop
-
-while b!=1: # Run loop until everything is right
-	i+=1 # increase everytime
-	print('This is try no. '+str(i))	
-	try:
-		start_resume() # do it
-	except:
-		pass # if error resume from here
-	else:
-		b=1
+start_resume()
